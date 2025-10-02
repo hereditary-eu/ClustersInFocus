4a610cd1bd7c4dde094cf627f8ff06f482ce166d
MrP01 <peter@waldert.at>
2025-10-02 11:24:02 +0200

    vega integration draft

--- frontend/src/components/DataVisualization/ScatterplotClustered.tsx
+++ frontend/src/components/DataVisualization/ScatterplotClustered.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+// import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import { useAppStore } from "../../stores/useAppStore";
 import { isNumericDataPoint } from "../../utils/validation";
+import { VegaEmbed } from "react-vega";
 
 interface ScatterplotClusteredProps {
   data: number[][];
@@ -59,58 +60,53 @@ const ScatterplotClustered: React.FC<ScatterplotClusteredProps> = ({
     setExpandedPanel("right");
   };
 
+  console.log(data);
+
   return (
     <div className="scatterplot-wrapper">
-      <ResponsiveContainer width={width} height={height}>
-        <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="x" name={xLabel} type="number" label={{ value: xLabel, position: "bottom", offset: 5 }} />
-          <YAxis
-            dataKey="y"
-            name={yLabel}
-            type="number"
-            label={{ value: yLabel, angle: -90, position: "left", offset: 5 }}
-          />
-          <Tooltip
-            cursor={{
-              strokeDasharray: "3 3",
-              stroke: "rgba(0, 0, 0, 0.2)",
-              strokeWidth: 1,
-              fill: "rgba(0, 0, 0, 0.05)",
-            }}
-            content={({ active, payload }) => {
-              if (active && payload && payload.length) {
-                return (
-                  <div className="scatter-tooltip">
-                    <p>{`${xLabel}: ${Number(payload[0].value).toFixed(4)}`}</p>
-                    <p>{`${yLabel}: ${Number(payload[0].payload.y).toFixed(4)}`}</p>
-                    <p>{`Cluster: ${Number(payload[0].payload.cluster) + 1}`}</p>
-                  </div>
-                );
-              }
-              return null;
-            }}
-          />
-          {/* <Legend /> */}
-          {clusterData.map((cluster, i) => {
-            const clusterId = actualClusterIds[i];
-            return (
-              <Scatter
-                key={clusterId}
-                name={`Cluster ${clusterId + 1}`}
-                data={cluster}
-                fill={colors[i % colors.length]} // Use modulo to avoid index errors
-                fillOpacity={0.6}
-                shape="circle"
-                r={4}
-                animationDuration={0}
-                onClick={() => handleClick(clusterId)}
-                cursor="pointer"
-              />
-            );
-          })}
-        </ScatterChart>
-      </ResponsiveContainer>
+      <VegaEmbed
+        spec={{
+          config: { view: { continuousWidth: 300, continuousHeight: 300, stroke: "transparent" } },
+          data: { name: "data" },
+          mark: { type: "point" },
+          encoding: {
+            color: { field: "cluster", type: "nominal" },
+            opacity: {
+              condition: { param: "param_3", value: 0.8, empty: true },
+              value: 0.2,
+            },
+            tooltip: { field: "Name", type: "nominal" },
+            x: { field: "x", type: "quantitative" },
+            y: {
+              field: "y",
+              title: "Y Label",
+              type: "quantitative",
+            },
+          },
+          params: [
+            {
+              name: "param_3",
+              select: { type: "point", fields: ["cluster"] },
+              bind: "legend",
+            },
+            {
+              name: "param_4",
+              select: { type: "interval", encodings: ["x", "y"] },
+              bind: "scales",
+            },
+          ],
+          $schema: "https://vega.github.io/schema/vega-lite/v5.20.1.json",
+          datasets: {
+            data: data.map((entry) => {
+              return {
+                x: entry[0],
+                y: entry[1],
+                cluster: entry[2],
+              };
+            }),
+          },
+        }}
+      />
     </div>
   );
 };
